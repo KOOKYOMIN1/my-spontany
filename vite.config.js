1799dc62ac79cdd34458f62f9aafe377bb9f8276
KYOMIN KOO <pgold97488@naver.com>
2025-05-23 15:25:36 +0900

    포트3543 으로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: '127.0.0.1', // ← localhost 대신 이걸로 강제
+    port: 3543,
+    host: '127.0.0.1',
   },
 })
