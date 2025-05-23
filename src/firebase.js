e43c5f6e5943358c71468be806b64410337e8470
KYOMIN KOO <pgold97488@naver.com>
2025-05-23 20:30:51 +0900

    Firebase 초기 연동

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD... (생략)",
+  authDomain: "my-spontany.firebaseapp.com",
+  projectId: "my-spontany",
+  storageBucket: "my-spontany.appspot.com",
+  messagingSenderId: "18373225769",
+  appId: "1:18373225769:web:3b2847313c304f8a54db38"
+  // measurementId: "G-XXXXX" // 안 써도 됨
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
