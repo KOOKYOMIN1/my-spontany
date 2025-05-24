bd0b997e8a4433c5837bff8ba9f787c9a733a9c2
KYOMIN KOO <pgold97488@naver.com>
2025-05-24 18:15:00 +0900

    history 삭제 기능 추가2

--- src/utils/saveUserPlan.js
+++ src/utils/saveUserPlan.js
@@ -1,12 +1,10 @@
-// src/utils/saveUserPlan.js
-
 import { collection, doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
 export const saveUserPlan = async (uid, planData) => {
   try {
-    const planId = Date.now().toString(); // 고유한 timestamp 기반 ID
-    const ref = doc(collection(doc(db, "plans", uid), "entries"), planId);
+    const planId = Date.now().toString(); // 고유 ID
+    const ref = doc(collection(doc(db, "plans", uid), "entries"), planId); // ✅ entries에 저장
     await setDoc(ref, planData);
     console.log("✅ Firestore 저장 성공");
   } catch (error) {
